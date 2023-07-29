4c5fd3a1ff607f0608c9b87cca09ac97ef21d373
YatishGurrala <gurralayatish@gmail.com>
2023-07-29 17:01:23 -0400

    Added_horizontalTechView_ in_Project_detail
    Created a custom  horizontal tech view in the project_detail class  and calling it in the main column of project_detail page

--- lib/example/projects.dart
+++ lib/example/projects.dart
@@ -12,21 +12,21 @@ List<Project> projects = [
       name: "Project 2",
       technologiesUsed: ["Next.js", "Firebase"],
       year: 2019,
-      description: "This is my description 1",
+      description: "This is my description 2",
       imageUrl:
           "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
   Project(
       name: "Project 3",
       technologiesUsed: ["Flutter", "Firebase"],
       year: 2020,
-      description: "This is my description 1",
+      description: "This is my description 3",
       imageUrl:
           "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
   Project(
       name: "Project 4",
       technologiesUsed: ["Next.js", "OAuth"],
       year: 2021,
-      description: "This is my description 1",
+      description: "This is my description 4",
       imageUrl:
           "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
 ];
